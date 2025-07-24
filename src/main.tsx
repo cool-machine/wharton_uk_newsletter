bc24fce97850645983074c68acb003774006b64e
Cool Machine <cool.lstm@gmail.com>
2025-07-24 19:08:21 +0200

    Fix blank page issue - redirect routes to actual newsletter components
    - Change root route from broken NewsletterForm to App component (March newsletter)
    - Route /templates to TestingPage instead of broken NewsletterForm
    - Import App component in main.tsx
    - Newsletter should now display properly instead of blank page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App.tsx';
 import AprilNewsletter from './AprilNewsletter.tsx';
 import NewsletterForm from './NewsletterForm.tsx';
 import TestingPage from './TestingPage.tsx';
@@ -10,33 +11,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route 
-          path="/" 
-          element={
-            <NewsletterForm 
-              colors={{ 
-                primary: '#990000', 
-                text: { 
-                  primary: '#333333' 
-                } 
-              }} 
-            />
-          } 
-        />
+        <Route path="/" element={<App />} />
         <Route path="/april" element={<AprilNewsletter />} />
-        <Route 
-          path="/templates" 
-          element={
-            <NewsletterForm 
-              colors={{ 
-                primary: '#990000', 
-                text: { 
-                  primary: '#333333' 
-                } 
-              }} 
-            />
-          } 
-        />
+        <Route path="/templates" element={<TestingPage />} />
         <Route path="/testing" element={<TestingPage />} />
       </Routes>
     </Router>
