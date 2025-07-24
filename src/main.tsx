76bdf6fd1eab26459734d80ec05afe8249332248
Cool Machine <cool.lstm@gmail.com>
2025-07-24 19:14:13 +0200

    Simplify app - remove React Router and show newsletter directly
    - Remove all routing complexity that may be causing blank page issues
    - Directly render App component (March newsletter)
    - This should eliminate any routing-related problems on GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import AprilNewsletter from './AprilNewsletter.tsx';
-import NewsletterForm from './NewsletterForm.tsx';
-import TestingPage from './TestingPage.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename="/newsletter_v3">
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/april" element={<AprilNewsletter />} />
-        <Route path="/templates" element={<TestingPage />} />
-        <Route path="/testing" element={<TestingPage />} />
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>
 );
