14876c3e38a526e79260e4d806c3d4d24955d542
Cool Machine <cool.lstm@gmail.com>
2025-07-24 20:49:19 +0200

    Deploy working newsletter with App component
    - Restore App component containing March 2024 newsletter
    - Clean build process with proper asset references
    - Newsletter should now display properly via JavaScript rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import SimpleTest from './SimpleTest.tsx';
-// import App from './App.tsx';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <SimpleTest />
+    <App />
   </StrictMode>
 );
